60be88cacdda3b715109ea7af652fde76be69be1
Steve <steven.g.ball@gmail.com>
2015-10-12 14:10:04 -0400

    Adding freetype font lib, adding basic rendering

---
+++ source/freetype/freetypefont.h
@@ -0,0 +1,56 @@
+// ******************************************************************************
+//
+// Filename:	freetypefont.h
+// Project:		OpenGLRenderer
+// Author:		Steven Ball
+//
+// Purpose:
+//   
+//
+// Revision History:
+//   Initial Revision - 11/10/08
+//
+// Copyright (c) 2005-2006, Steven Ball
+//
+// ******************************************************************************
+
+#pragma once
+
+#include <windows.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+
+class FreeTypeFont {
+public:
+	FreeTypeFont();
+	~FreeTypeFont();
+
+	void BuildFont(const char* fontName, int size);
+	void DrawString(const char *text, float scale);
+
+	int GetTextWidth(const char *text);
+	int GetCharWidth(int c);
+	int GetCharHeight(int c);
+	int GetAscent();
+	int GetDescent();
+
+protected:
+	void MakeDisplayList(FT_Face face, unsigned char ch, GLuint list_base, GLuint * tex_base);
+
+private:
+
+	bool m_inited;
+
+	FT_Library m_library;
+
+	FT_Face m_face;
+
+	int m_size;
+
+	GLuint* m_textures;
+	GLuint m_base;
+};
--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,14 @@
 #include <GLFW/glfw3.h>
 
+#include "freetypefont.h"
+
+#include <windows.h>
+#include <gl/gl.h>
+#include <gl/glu.h>
+
+#pragma comment (lib, "opengl32")
+#pragma comment (lib, "glu32")
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -31,13 +40,35 @@ int main(void)
 	glfwSwapInterval(1);
 
 	/* Create fonts */
-	//FreeTypeFont* font = new FreeTypeFont();
+	FreeTypeFont* font = new FreeTypeFont();
+	font->BuildFont("media/fonts/arial.ttf", 18);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
+		float ratio;
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		ratio = width / (float)height;
+
+		glViewport(0, 0, width, height);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(-ratio, ratio, -1.0f, 1.0f, 1.0f, -1.0f);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+
+		glBegin(GL_TRIANGLES);
+			glColor3f(1.0f, 0.0f, 0.0f);
+			glVertex3f(-0.6f, -0.4f, 0.0);
+			glColor3f(0.0f, 1.0f, 0.0f);
+			glVertex3f(0.6f, -0.4f, 0.0f);
+			glColor3f(0.0f, 0.0f, 1.0f);
+			glVertex3f(0.0f, 0.6f, 0.0f);
+		glEnd();
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
