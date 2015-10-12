295c5b2b1a73f2aaa54079d3c6e20a57725f1d74
Steve <steven.g.ball@gmail.com>
2015-10-12 11:54:04 -0400

    remove console from window. add opengl link, better exit codes

--- main.cpp
+++ main.cpp
@@ -1,28 +1,33 @@
 #include <GLFW/glfw3.h>
 
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
 {
 	GLFWwindow* window;
 
 	/* Initialize the library */
 	if (!glfwInit())
-		return -1;
+		exit(EXIT_FAILURE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "Vox", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
-		return -1;
+		exit(EXIT_FAILURE);
 	}
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
@@ -32,6 +37,5 @@ int main()
 	}
 
 	glfwTerminate();
-
-	return 0;
+	exit(EXIT_SUCCESS);
 }
